a31b80936fa4f903f64a55329b8e953c5ab58a9f
XuSui <940700045@qq.com>
2026-01-28 10:55:41 +0800

    KMP-nextval.version

--- DataStructuresAndAlgorithms/KMP.c
+++ DataStructuresAndAlgorithms/KMP.c
@@ -5,8 +5,10 @@
 #include <string.h>
 
 char* KMP(const char* str, const char* pattern) {
+	int sz = strlen(pattern);
+
 	//NEXT
-	int* next = (int*)calloc(strlen(pattern), sizeof(int));
+	int* next = (int*)calloc(sz, sizeof(int));
 	next[0] = 0;
 	for (int i = 1, j = 0; pattern[i] != '\0';) {
 		while (j > 0 && pattern[i] != pattern[j])
@@ -16,20 +18,35 @@ char* KMP(const char* str, const char* pattern) {
 	
 	//print NEXT
 	printf("next >> ");
-	for (int i = 0; i < strlen(pattern); printf("%-2d", next[i++]));
+	for (int i = 0; i < sz; printf("%-2d", next[i++]));
+	printf("\n");
+
+	//NEXTVAL
+	int* nextval = (int*)calloc(sz, sizeof(int));
+	for (int i = 1; pattern[i] != '\0'; i++) {
+		if (next[i - 1] > 0 && pattern[i] == pattern[next[i - 1]])
+			nextval[i - 1] = nextval[next[i - 1] - 1];
+		else
+			nextval[i - 1] = next[i - 1];
+	}
+	nextval[sz - 1] = 0;
+
+	//print NEXTVAL
+	printf("nextval >> ");
+	for (int i = 0; i < sz; printf("%-2d", nextval[i++]));
 	printf("\n");
 
 	//MATCH
 	for (int i = 0, j = 0; str[i] != '\0'; i++) {
-		while (j > 0 && str[i] != pattern[j])
-			j = next[j - 1];
+		if (j > 0 && str[i] != pattern[j])
+			j = nextval[j - 1];
 		if (str[i] == pattern[j])
-			if ((++j) == strlen(pattern)) {
-				free(next);
+			if ((++j) == sz) {
+				free(next), free(nextval);
 				return str + i - j + 1;
 			}
 	}
-	free(next);
+	free(next), free(nextval);
 	return NULL;
 }
 
